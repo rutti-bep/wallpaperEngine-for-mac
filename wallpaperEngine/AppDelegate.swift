af4e4e5d68dcc8442823a7781905eddc35ad49b6
rutti-bep <konnosatoru@konnoakira-no-MacBook.local>
2017-02-08 16:14:52 +0900

    とりあえずできた

--- wallpaperEngine/AppDelegate.swift
+++ wallpaperEngine/AppDelegate.swift
@@ -9,25 +9,79 @@
 import Cocoa
 //import Dispatch
 //import CoreGraphics
-import Foundation
 import AppKit
+import AVKit
+import Foundation
+import AVFoundation
+
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate{
 
     @IBOutlet weak var window: NSWindow!
-    let imageUrls = ["/Users/konnosatoru/Downloads/Cg5A7_7U0AAWpwA.jpg",
+    @IBOutlet weak var playerView: AVPlayerView!
+    
+    var imageUrls = [URL]();
+   /*     "/Users/konnosatoru/Downloads/Cg5A7_7U0AAWpwA.jpg",
                      "/Users/konnosatoru/Downloads/graphic23_l.png"
-    ];
+    ];*/
+    var imagesDir =  NSURL.fileURL(withPath: "/Users/konnosatoru/Desktop/wallpaper/");
     var count = 0;
     
+    
+    func start(){
+        let documentsDirectoryURL =  NSURL.fileURL(withPath: "/Users/konnosatoru/Desktop/wallpaper/");
+        var bool: ObjCBool = false
+        if FileManager.default.fileExists(atPath: documentsDirectoryURL.path, isDirectory: &bool),bool.boolValue  {
+            print("url is a folder url")
+            do {
+                let files = try FileManager.default.contentsOfDirectory(at: documentsDirectoryURL, includingPropertiesForKeys: nil, options: [])
+                let picFiles = files.filter{ $0.pathExtension == "png" }
+                for picUrl in picFiles{
+                    imageUrls += [ picUrl ];
+                    //print("file urls:",picUrl)
+                }
+                
+            } catch let error as NSError {
+                print(error.localizedDescription + "ok")
+            }
+        }
+    }
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        var wallpaperTimer = Timer.scheduledTimer(
-            timeInterval: 0.03,
+        start()
+        let screen = NSScreen.main()
+        
+        // パスからassetを生成.
+        let path = "/Users/konnosatoru/Movies/小林さんちのメイドラゴン/小林さんちのメイドラゴン OP.mp4"
+        
+        let fileURL: URL = NSURL.fileURL(withPath: path)
+        print(fileURL)
+        let avAsset = AVURLAsset(url: fileURL)
+        // AVPlayerに再生させるアイテムを生成.
+        let Item = AVPlayerItem(asset: avAsset)
+        
+        var player = AVPlayer(playerItem: Item)
+        
+        playerView.player = player
+       
+        player.play()
+        
+        window.level = Int(CGWindowLevelForKey(.desktopIconWindow))
+        window.styleMask = NSBorderlessWindowMask
+        if let frame = screen?.frame  {
+            let size = NSSize(width: frame.size.width, height: frame.size.height)
+            let point = NSPoint(x: 0, y: 0)
+            window.setFrameOrigin(point)
+            window.setContentSize(size)
+            print(size)
+        }
+    /*    var wallpaperTimer = Timer.scheduledTimer(
+            timeInterval: 0.01,
             target: self,
             selector: Selector("wallpaperChange"),
             userInfo: nil,
-            repeats: true);
+            repeats: true);*/
     }
     
     func wallpaperChange(){
@@ -37,7 +91,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
         print(count)
         do {
-            let imgurl = NSURL.fileURL(withPath: imageUrls[count])
+            var imgurl = imageUrls[count];
+            print (imgurl)
             let workspace = NSWorkspace.shared()
             if let screen = NSScreen.main()  {
                 try workspace.setDesktopImageURL(imgurl, for: screen, options: [:])
