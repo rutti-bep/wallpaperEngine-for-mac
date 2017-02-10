478b6ca065860cb0793a4eba1c7ab2a2b0772ecf
rutti-bep <konnosatoru@konnoakira-no-macbook.local>
2017-02-10 15:22:24 +0900

    menuViewを整理した

--- wallpaperEngine/AppDelegate.swift
+++ wallpaperEngine/AppDelegate.swift
@@ -18,12 +18,20 @@ class AppDelegate: NSObject, NSApplicationDelegate{
 
     @IBOutlet weak var window: NSWindow!
     @IBOutlet weak var playerView: AVPlayerView!
+    var player = AVPlayer()
+    
     var statusItem = NSStatusBar.system().statusItem(withLength: -1)
     let menu = NSMenu()
     let menuItemRepeat = NSMenuItem()
-    let popover = NSPopover()
-    var player = AVPlayer()
-    var isMovieRepeat = false
+    let popovar = NSPopover()
+    var popupViewController = NSViewController()
+    var quitButton = NSButton()
+    var selectButton = NSButton()
+    var repeatButton = NSButton()
+    var moviePlayTimeText = NSText()
+    var seekBar = NSSlider()
+    
+ //   var isMovieRepeat = false
     
     func startMovie(fileURL: URL){
         let avAsset = AVURLAsset(url: fileURL)
@@ -31,6 +39,14 @@ class AppDelegate: NSObject, NSApplicationDelegate{
         player = AVPlayer(playerItem: Item)
         playerView.player = player
         player.play()
+        seekBar.action = #selector(AppDelegate.onSeekbarValueChange)
+        seekBar.minValue = 0
+        seekBar.maxValue = CMTimeGetSeconds(avAsset.duration)
+    }
+
+    func onSeekbarValueChange(){
+        let seekvarValue:Float64 = Float64(seekBar.floatValue)
+        player.seek(to: CMTimeMakeWithSeconds(seekvarValue,Int32(NSEC_PER_SEC)))
     }
     
     func setWindow(){
@@ -44,7 +60,6 @@ class AppDelegate: NSObject, NSApplicationDelegate{
             let point = NSPoint(x: 0, y: 0)
             window.setFrameOrigin(point)
             window.setContentSize(size)
-            //print(size)
         }
         
     }
@@ -55,7 +70,7 @@ class AppDelegate: NSObject, NSApplicationDelegate{
         openPanel.canChooseDirectories = false
         openPanel.canCreateDirectories = false
         openPanel.canChooseFiles = true
-        openPanel.allowedFileTypes = ["mp4","m4a"]
+        openPanel.allowedFileTypes = ["mp4","mov","m4v"]
         let path = NSSearchPathForDirectoriesInDomains(.moviesDirectory, .userDomainMask, true)[0] as String
         let url:URL = NSURL(fileURLWithPath: path) as URL
         openPanel.directoryURL = url;
@@ -67,18 +82,27 @@ class AppDelegate: NSObject, NSApplicationDelegate{
                 // ここでファイルを読み込む
             }
         }
-      //  let workspace = NSWorkspace.shared()
        
     }
     
-    func setRepeatMovie(){
-        isMovieRepeat = !isMovieRepeat
-        if(isMovieRepeat){
-            menuItemRepeat.title = "repeat ✔︎"
-        }else{
-            menuItemRepeat.title = "repeat"
+    func togglePopover(_ sender: AnyObject?) {
+        if popovar.isShown {
+            closePopover(sender)
+        } else {
+            showPopover(sender)
         }
     }
+    
+    func showPopover(_ sender: AnyObject?) {
+        if let button = statusItem.button {
+            popovar.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+        }
+    }
+    
+    func closePopover(_ sender: AnyObject?) {
+        popovar.performClose(sender)
+    }
+
     
     func quit(){
         exit(0);
@@ -88,28 +112,57 @@ class AppDelegate: NSObject, NSApplicationDelegate{
         self.setWindow()
         self.launchFinder()
         
-        self.statusItem.title = "wallpaper!"
-        self.statusItem.highlightMode = true
-        self.statusItem.menu = menu
-        
         NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: self.player.currentItem, queue: nil, using: { (_) in
             DispatchQueue.main.async {
-                if(self.isMovieRepeat){
+                if(self.repeatButton.state == 1){
                     self.player.seek(to: kCMTimeZero)
                     self.player.play()
                 }
             }
         })
-      /* NSWorkspace.shared().notificationCenter.addObserver(self,
-                                                            selector: #selector(AppDelegate.setWindow),
-                                                                     name: NSNotification.Name.NSWorkspaceActiveSpaceDidChange,
-                                                                     object: nil)
-       */
-       /* if let button = self.statusItem.button {
-            button.title = "pop up!";
-            button.action = #selector(AppDelegate.togglePopover)
-            print("pop")
-        }*/
+        
+        self.statusItem.title = "wallpaper!"
+        
+        if let button = self.statusItem.button {
+            button.image = NSImage(named: "StatusBarButtonImage")
+            button.action = #selector(AppDelegate.togglePopover(_:))
+        }
+        
+        let frame = NSRect.init(x: 0, y: 0, width: 300, height: 200)
+        let popupView = NSView.init(frame: frame)
+        popupViewController.view = popupView
+        
+        quitButton = NSButton(frame: NSRect(x: 0, y: 0, width: 100, height: 20))
+        quitButton.frame.origin = NSPoint(x: frame.maxX-100, y: 180)
+        quitButton.title = "quit"
+        quitButton.action = #selector(AppDelegate.quit)
+        popupView.addSubview(quitButton)
+        (quitButton.cell as! NSButtonCell).backgroundColor = NSColor.red
+        
+        selectButton = NSButton(frame: NSRect(x: 0, y: 0, width: 100, height: 20))
+        selectButton.frame.origin = NSPoint(x: 0, y: 180)
+        selectButton.title = "select"
+        selectButton.action = #selector(AppDelegate.launchFinder)
+        popupView.addSubview(selectButton)
+        
+        seekBar = NSSlider(frame: NSRect(x: 0, y: 0, width: frame.maxX - 100, height : 20))
+        seekBar.frame.origin = NSPoint(x: 0, y: 10)
+       // seekBar.addTarget(self, action: "onSliderValueChange:", forControlEvents: UIControlEvents.ValueChanged)
+        popupView.addSubview(seekBar)
+        
+        repeatButton = NSButton(frame: NSRect(x: 0, y: 0, width: 100, height: 20))
+        repeatButton.title = "repeat"
+        repeatButton.setButtonType(NSSwitchButton)
+        repeatButton.state = 0
+      //  repeatButton.action = #selector(AppDelegate.setRepeatMovie)
+       // print(repeatButton.cell?.type)
+        repeatButton.frame.origin = NSPoint(x: 200, y: 10)
+        popupView.addSubview(repeatButton)
+        
+        popovar.contentViewController = popupViewController
+        
+     /*   self.statusItem.highlightMode = true
+        self.statusItem.menu = menu
         
         let menuItemSelect = NSMenuItem()
         menuItemSelect.title = "select"
@@ -124,33 +177,9 @@ class AppDelegate: NSObject, NSApplicationDelegate{
         menuItemQuit.title = "quit"
         menuItemQuit.action = #selector(AppDelegate.quit)
         menu.addItem(menuItemQuit)
-    
-      //  popover.contentViewController = NSTabViewController(nibName: "QuotesViewController", bundle: nil)
+      */
         
-       
     }
-    
-    func showPopover() {
-        if let button = self.statusItem.button {
-            
-            print(button.bounds)
-            popover.show(relativeTo:button.bounds , of: button, preferredEdge:  NSRectEdge.minY)
-        }
-    }
-    
-    func closePopover() {
-     //   popover.performClose(sender)
-    }
-    
-    func togglePopover() {
-        print("hoge")
-        if popover.isShown {
-            closePopover()
-        } else {
-            showPopover()
-        }
-    }
-    
   
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
